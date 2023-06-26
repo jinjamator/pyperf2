2a00f12c838a91b39ebce0c36af84157b69f63d1
Andreas Ueberbacher <andreas.ueberbacher@k-business.com>
2023-06-26 16:08:58 +0200

    initial tcp support

--- pyperf2/__init__.py
+++ pyperf2/__init__.py
@@ -15,7 +15,6 @@ import datetime
 from pyroute2 import netns
 
 
-
 def output_reader(proc, outq, parent):
     for line in iter(proc.stdout.readline, b""):
         outq.put(line.decode("utf-8"))
@@ -24,8 +23,10 @@ def output_reader(proc, outq, parent):
 
         parent.line_ready_callback()
 
+
 class NameSpaceNotFoundError(Exception):
     pass
+
 
 class IPerfInstance(object):
     def __init__(self):
@@ -59,7 +60,7 @@ class IPerfInstance(object):
         self._raw_log_filepath = None
         self._raw_log_filehandler = None
         self._creation_time = datetime.datetime.now().replace(microsecond=0).isoformat()
-        self.ttl=255
+        self.ttl = 255
 
         if "pps" in self.bandwidth:
             self.expected_interval_packets = int(
@@ -101,8 +102,11 @@ class IPerfInstance(object):
             self._raw_log_filehandler.write(line)
             self._raw_log_filehandler.flush()
         if not self._info_regex:
-            raise RuntimeError("missing regex for parsing output",pformat(self.generate_cli_from_options()))
-        
+            raise RuntimeError(
+                "missing regex for parsing output",
+                pformat(self.generate_cli_from_options()),
+            )
+
         result = self._info_regex.match(line)  # check if it's an info header
         if result:
             info_data = result.groupdict()
@@ -448,7 +452,9 @@ class IPerfInstance(object):
         _cli = []
         if self.use_linux_namespace:
             if self.use_linux_namespace not in list(netns.listnetns()):
-                raise NameSpaceNotFoundError(f"Namespace {self.use_linux_namespace} cannot be found.")
+                raise NameSpaceNotFoundError(
+                    f"Namespace {self.use_linux_namespace} cannot be found."
+                )
             _cli.extend("ip netns exec {0}".format(self.use_linux_namespace).split(" "))
         _cli.append(self.iperf_binary_path)
         _cli.append("-e")
@@ -460,6 +466,12 @@ class IPerfInstance(object):
             if self.bind_ip:
                 _cli.append("-B")
                 _cli.append(self.bind_ip)
+            ####Ü
+            if self.protocol == "tcp":
+                self._result_regex = re.compile(
+                    r"^\[\s*(?P<stream_id>\d+)\]\s+(?P<interval_begin>\d+\.\d+)-(?P<interval_end>\d+\.\d+)\s+(?P<interval_unit>\S+)\s+(?P<received>\S+)\s+(?P<received_unit>\S+)\s+(?P<bandwidth>\S+)\s+(?P<bandwidth_unit>\S+)?"
+                )
+
             if self.protocol == "udp":
                 # multicast server result
                 # [  4] 2.6000-2.7000 sec  0.00 Bytes   0.00 bits/sec    0.000 ms    0/    0 (0%)  -/-/-/-                    ms    0 pps
@@ -477,12 +489,17 @@ class IPerfInstance(object):
                 _cli.append("-B")
                 _cli.append("{0}:{1}".format(self.bind_ip, self.client_source_port))
             if self.protocol == "udp":
-                _cli.append("-l")
-                _cli.append(self.packet_len)
                 # [  3] local 192.168.51.154 port 54877 connected with 225.0.0.5 port 5001
                 self._result_regex = re.compile(
                     r"^\[\s*(?P<stream_id>\d+)\]\s+(?P<interval_begin>\d+\.\d+)-(?P<interval_end>\d+\.\d+)\s+(?P<interval_unit>\S+)\s+(?P<transferred>\S+)\s+(?P<transferred_unit>\S+)\s+(?P<bandwidth>\S+)\s+(?P<bandwidth_unit>\S+)\s+(?P<packets_written>\S+)/(?P<packets_error>\S+)\s+(?P<packet_rate>\d+)\s+(?P<packet_rate_unit>\S+)"
                 )
+            if self.protocol == "tcp":
+                self._result_regex = re.compile(
+                    r"^\[\s*(?P<stream_id>\d+)\]\s+(?P<interval_begin>\d+\.\d+)-(?P<interval_end>\d+\.\d+)\s+(?P<interval_unit>\S+)\s+(?P<transferred>\S+)\s+(?P<transferred_unit>\S+)\s+(?P<bandwidth>\S+)\s+(?P<bandwidth_unit>\S+)\s+(?P<packets_written>\S+)/(?P<packets_error>\S+)\s+(?P<retry>\d+)\s+(?P<cwnd>\S+)/(?P<rtt>\d+)\S(?P<var>\d+)\W*(?P<var_unit>\S+)\W*(?P<netpower>\d+\S*)"
+                )
+            if self.packet_len:
+                _cli.append("-l")
+                _cli.append(self.packet_len)
             if self.ttl:
                 _cli.append("-T")
                 _cli.append(str(self.ttl))
@@ -491,6 +508,7 @@ class IPerfInstance(object):
             raise ValueError("type must be set to either server or client")
         if self.protocol == "udp":
             _cli.append("-u")
+        if self.protocol == "udp" or "tcp":
             self._info_regex = re.compile(
                 r"^\[\s*(?P<stream_id>\d+)\]\s+local\s+(?P<local_ip>\S+)\s+port\s+(?P<local_port>\S+)\s+connected\s+with\s+(?P<remote_ip>\S+)\s+port\s+(?P<remote_port>\S+)"
             )
@@ -502,7 +520,14 @@ class IPerfInstance(object):
         _cli.append(self.report_interval)
 
         _cli.append("-b")
-        _cli.append(self.bandwidth)
+        if self.protocol == "tcp":
+            if "pps" not in self.bandwidth:
+                _cli.append(self.bandwidth)
+            else:
+                _cli.append("1M")
+
+        if self.protocol == "udp":
+            _cli.append(self.bandwidth)
 
         _cli.append("-p")
         _cli.append(self.port)
